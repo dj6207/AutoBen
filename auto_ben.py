214e63c27380d0bcf4df3965e7f3170111dd0912
Devin <dj6207@rit.edu>
2023-10-04 00:10:37 -0400

    ben

--- auto_ben.py
+++ auto_ben.py
@@ -1,15 +1,30 @@
 import pyautogui as pya
 import logging
 import queue
+import time
+import os
+import datetime
 
-# Clicking - Hashmap with all the known locations 
-# The name of the location will be the keys and the location in x, y coordinates will be the values
-# Wrapper function that returns the coordinates of the location name
-# Queue that will queue up the list of commands
-# Class for commands
-# Writing the recorded time into a file
+SLEEP_TIME = 1
 
 CHANGE_CONSTANT = 0.5
+
+COMMAND_LIST = [
+    ("SINE", None),
+    ("AMPLITUDE_UP", 0),
+    ("FREQUENCY_UP", 2),
+    ("RECORD_TIME", None),
+    ("WAIT", 1),
+    ("LL", None),
+    ("RECORD_TIME", None),
+    ("WAIT", 1),
+    ("V1", None),
+    ("RECORD_TIME", None),
+    ("WAIT", 1),
+    ("RA", None),
+    ("AMPLITUDE_UP", 2),
+    ("RECORD_TIME",None),
+]
 
 LOCATIONS = {
     "SINE": (0, 0),
@@ -28,18 +43,50 @@ class Command:
         self.change = change
 
     def execute(self):
-        button_press = self.change/CHANGE_CONSTANT
-        location = LOCATIONS.get(self.name, None)
-        if location is not None:
-            for _ in button_press:
-                pya.click(location)
+        if self.name == "RECORD_TIME":
+            print("Recording time")
+            logging.info("Recording time")
+        elif self.name == "WAIT":
+            print(f"Sleeping for {SLEEP_TIME}")
+            logging.info(f"Sleeping for {SLEEP_TIME}")
+            time.sleep(SLEEP_TIME)
+        else:
+            button_press = self.change/CHANGE_CONSTANT if self.change else 1
+            location = LOCATIONS.get(self.name, None)
+            if location is not None:
+                for _ in range(int(button_press)):
+                    # pya.click(location)
+                    print(f"Going to {location}")
+                    logging.info(f"Going to {location}")
+
+def print_queue(queue):
+    while not queue.empty():
+        print(queue.get().name)
 
 def initialize_queue():
-    
-    return
+    command_queue = queue.Queue()
+    for name, change in COMMAND_LIST:
+        command_queue.put(Command(name, change))
+    return command_queue
+
+def initialize_logs():
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    logs_folder_path = os.path.join(desktop_path, "logs")
+    if not os.path.exists(logs_folder_path):
+        os.makedirs(logs_folder_path)
+    current_time = datetime.datetime.now()
+    timestamp_str = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+    return os.path.join(logs_folder_path, f"log_{timestamp_str}.log")
 
 def main():
-    initialize_queue()
+    logging.basicConfig(
+        filename=initialize_logs(), 
+        level=logging.INFO, 
+        format='%(asctime)s - %(message)s'
+    )
+    command_queue = initialize_queue()
+    while not command_queue.empty():
+        command_queue.get().execute()
 
 if __name__ == "__main__":
     main()
