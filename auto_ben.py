9d0c09af9f9ed7cad30b853e3e07f1c2ecdbfaf5
Devin <dj6207@rit.edu>
2023-10-06 19:42:16 -0400

    Ben GUI

--- auto_ben.py
+++ auto_ben.py
@@ -6,97 +6,20 @@ import os
 import datetime
 import winsound
 
-VERSION = 1.0
+VERSION = 1.2
 
-# How much 
+LOGS_FOLDER = os.path.join(os.path.join(os.path.expanduser("~"), "Desktop"), "logs")
 CHANGE_AMPLITUDE_CONSTANT = 0.1
 CHANGE_PACING_AMPLITUDE_CONSTANT = 2
-WAIT_TIME = 1
-START_DELAY = 1
-
-COMMAND_LIST = [
-    ("SINE", None),
-    ("AMPLITUDE_DOWN", 0), # 0mV
-    ("FREQUENCY_UP", 2), # 0Hz - 2Hz
-    ("RECORD_TIME", None),
-    ("WAIT", WAIT_TIME),
-    ("RA", None),
-    ("LL", None),
-    ("RECORD_TIME", None),
-    ("WAIT", WAIT_TIME),
-    ("LL", None),
-    ("V1", None),
-    ("RECORD_TIME", None),
-    ("WAIT", WAIT_TIME),
-    ("V1", None),
-    ("RA", None),
-    ("AMPLITUDE_UP", 2), # 0mV - 2mV
-
-    ("RECORD_TIME",None),
-    ("WAIT", WAIT_TIME),
-    ("RA", None),
-    ("LL", None),
-    ("RECORD_TIME",None),
-    ("WAIT", WAIT_TIME),
-    ("LL", None),
-    ("V1", None),
-    ("RECORD_TIME",None),
-    ("WAIT", WAIT_TIME),
-    ("V1", None),
-    ("RA", None),
-
-    ("FREQUENCY_UP", 10), # 2Hz - 10Hz
-    ("RECORD_TIME",None),
-
-    ("WAIT", WAIT_TIME),
-    ("RA", None),
-    ("LL", None),
-    ("RECORD_TIME",None),
-    ("WAIT", WAIT_TIME),
-    ("LL", None),
-    ("V1", None),
-    ("RECORD_TIME",None),
-    ("WAIT", WAIT_TIME),
-    ("V1", None),
-    ("RA", None),
-
-    ("FREQUENCY_DOWN", 5), # 10Hz - 5Hz
-    ("WAIT", WAIT_TIME),
-    ("PACING_AMPLITUDE_UP", 2),
-
-    ("RECORD_TIME",None),
-    ("WAIT", WAIT_TIME),
-    ("RA", None),
-    ("LL", None),
-    ("RECORD_TIME",None),
-    ("WAIT", WAIT_TIME),
-    ("LL", None),
-    ("V1", None),
-    ("RECORD_TIME",None),
-    ("WAIT", WAIT_TIME),
-    ("V1", None),
-    ("RA", None),
-]
-
-LOCATIONS = {
-    "SINE": (457, 217),
-    "RA": (955, 189),
-    "AMPLITUDE_UP": (709, 204),
-    "AMPLITUDE_DOWN": (708, 219),
-    "PACING_AMPLITUDE_UP": (869, 486),
-    "PACING_AMPLITUDE_DOWN": (870, 501),
-    "FREQUENCY_UP": (706, 264),
-    "FREQUENCY_DOWN": (707, 281),
-    "LL": (950, 234),
-    "V1":(953, 260),
-}
+WAIT_TIME = 60
+START_DELAY = 15
 
 class Command:
     def __init__(self, name, change):
         self.name = name
         self.change = change
 
-    def execute(self, current_frequency, current_amplitude, current_pacing_amplitude):
+    def execute(self, current_frequency, current_amplitude, current_pacing_amplitude, locations):
         if self.name == "RECORD_TIME":
             print("Recording time")
             logging.info("Recording time")
@@ -106,7 +29,7 @@ class Command:
             time.sleep(self.change)
         else:
             button_press = 1
-            # Amplitude starts a 1mV and changesby 0.1
+            # Amplitude starts a 1mV and changes by 0.1
             if self.name.split("_")[0] == "AMPLITUDE":
                 button_press = abs((self.change - current_amplitude))/CHANGE_AMPLITUDE_CONSTANT if self.change is not None else 1
                 current_amplitude = self.change if self.change is not None else current_amplitude
@@ -118,7 +41,7 @@ class Command:
             else:
                 button_press = abs((self.change - current_pacing_amplitude))/CHANGE_PACING_AMPLITUDE_CONSTANT if self.change is not None else 1
                 current_pacing_amplitude = self.change if self.change is not None else current_pacing_amplitude
-            location = LOCATIONS.get(self.name, None)
+            location = locations.get(self.name, None)
             if location is not None:
                 logging.info(f"Pressed {int(button_press)} times")
                 for _ in range(int(button_press)):
@@ -127,19 +50,97 @@ class Command:
                     logging.info(f"Clicking {self.name} at {location}")
         return current_frequency, current_amplitude, current_pacing_amplitude
 
-def initialize_queue():
+def initialize_queue(command_list):
     command_queue = queue.Queue()
-    for name, change in COMMAND_LIST:
+    for name, change in command_list:
         command_queue.put(Command(name, change))
     return command_queue
 
 def initialize_logs():
-    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-    logs_folder_path = os.path.join(desktop_path, "logs")
-    if not os.path.exists(logs_folder_path):
-        os.makedirs(logs_folder_path)
+
+    if not os.path.exists(LOGS_FOLDER):
+        os.makedirs(LOGS_FOLDER)
     timestamp_str = datetime.datetime.now().strftime("%m-%d-%Y_%H.%M.%S")
-    return os.path.join(logs_folder_path, f"{timestamp_str}.log")
+    return os.path.join(LOGS_FOLDER, f"{timestamp_str}.log")
+
+def initialize_commands():
+    return [
+        ("SINE", None),
+        ("AMPLITUDE_DOWN", 0), # 0mV
+        ("FREQUENCY_UP", 2), # 0Hz - 2Hz
+        ("RECORD_TIME", None),
+        ("WAIT", WAIT_TIME),
+        ("RA", None),
+        ("LL", None),
+        ("RECORD_TIME", None),
+        ("WAIT", WAIT_TIME),
+        ("LL", None),
+        ("V1", None),
+        ("RECORD_TIME", None),
+        ("WAIT", WAIT_TIME),
+        ("V1", None),
+        ("RA", None),
+        ("AMPLITUDE_UP", 2), # 0mV - 2mV
+
+        ("RECORD_TIME",None),
+        ("WAIT", WAIT_TIME),
+        ("RA", None),
+        ("LL", None),
+        ("RECORD_TIME",None),
+        ("WAIT", WAIT_TIME),
+        ("LL", None),
+        ("V1", None),
+        ("RECORD_TIME",None),
+        ("WAIT", WAIT_TIME),
+        ("V1", None),
+        ("RA", None),
+
+        ("FREQUENCY_UP", 10), # 2Hz - 10Hz
+        ("RECORD_TIME",None),
+
+        ("WAIT", WAIT_TIME),
+        ("RA", None),
+        ("LL", None),
+        ("RECORD_TIME",None),
+        ("WAIT", WAIT_TIME),
+        ("LL", None),
+        ("V1", None),
+        ("RECORD_TIME",None),
+        ("WAIT", WAIT_TIME),
+        ("V1", None),
+        ("RA", None),
+
+        ("FREQUENCY_DOWN", 5), # 10Hz - 5Hz
+        ("WAIT", WAIT_TIME),
+        ("PACING_AMPLITUDE_UP", 2),
+
+        ("RECORD_TIME",None),
+        ("WAIT", WAIT_TIME),
+        ("RA", None),
+        ("LL", None),
+        ("RECORD_TIME",None),
+        ("WAIT", WAIT_TIME),
+        ("LL", None),
+        ("V1", None),
+        ("RECORD_TIME",None),
+        ("WAIT", WAIT_TIME),
+        ("V1", None),
+        ("RA", None),
+    ]
+
+def initialize_locations():
+    return {
+        "SINE": (457, 217),
+        "RA": (955, 189),
+        "AMPLITUDE_UP": (709, 204),
+        "AMPLITUDE_DOWN": (708, 219),
+        "PACING_AMPLITUDE_UP": (869, 486),
+        "PACING_AMPLITUDE_DOWN": (870, 501),
+        "FREQUENCY_UP": (706, 264),
+        "FREQUENCY_DOWN": (707, 281),
+        "LL": (950, 234),
+        "V1":(953, 260),
+    }
 
 def main():
     frequency = 1
@@ -151,13 +152,13 @@ def main():
         format='%(asctime)s: %(message)s',
         datefmt="%m-%d-%Y %H.%M.%S"
     )
-    command_queue = initialize_queue()
+    command_queue = initialize_queue(initialize_commands())
     print(f"Auto_Ben Version {VERSION}")
     logging.info(f"Auto_Ben Version {VERSION}")
     print(f"Script will Start in {START_DELAY} Seconds")
     time.sleep(START_DELAY)
     while not command_queue.empty():
-        frequency, amplitude, pacing_amplitude = command_queue.get().execute(frequency, amplitude, pacing_amplitude)
+        frequency, amplitude, pacing_amplitude = command_queue.get().execute(frequency, amplitude, pacing_amplitude, initialize_locations())
         logging.info(f"Frequency: {frequency}, Amplitude: {amplitude}, Pacing Amplitude: {pacing_amplitude}")
     winsound.PlaySound("sound.wav", winsound.SND_FILENAME)
 
---
+++ auto_ben_gui.py
@@ -0,0 +1,51 @@
+import auto_ben
+import tkinter as tk
+from tkinter import filedialog
+
+class AutoBenGUI(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Hi Ben")
+
+        self.wait_time_label = tk.Label(self, text="Wait Time:")
+        self.wait_time_label.grid(row=0, column=0)
+        self.wait_time_entry = tk.Entry(self)
+        self.wait_time_entry.grid(row=0, column=1)
+        self.wait_time_entry.insert(0, auto_ben.WAIT_TIME)
+
+        self.start_delay_label = tk.Label(self, text="Start Delay:")
+        self.start_delay_label.grid(row=1, column=0)
+        self.start_delay_entry = tk.Entry(self)
+        self.start_delay_entry.grid(row=1, column=1)
+        self.start_delay_entry.insert(0, auto_ben.START_DELAY)
+
+        self.log_location_path = tk.Entry(self)
+        self.log_location_path.grid(row=2, column=0, columnspan=2, sticky='WE')
+        self.log_location_path.insert(0, auto_ben.LOGS_FOLDER)
+        self.log_location_path.config(state="readonly")
+        self.log_location_button = tk.Button(self, text="Select Log Location", command=self.select_log_location)
+        self.log_location_button.grid(row=3, column=0, columnspan=2, sticky='WE')
+
+        self.start_button = tk.Button(self, text="Start", command=self.start_main)
+        self.start_button.grid(row=4, column=0, columnspan=2, sticky='WE')
+
+    def select_log_location(self):
+        directory = filedialog.askdirectory()
+        if directory:
+            self.log_location_path.config(state=tk.NORMAL)
+            self.log_location_path.delete(0, tk.END)
+            self.log_location_path.insert(0, directory)
+            self.log_location_path.config(state="readonly")
+
+    def start_main(self):
+        auto_ben.WAIT_TIME = int(self.wait_time_entry.get())
+        auto_ben.START_DELAY = int(self.start_delay_entry.get())    
+        auto_ben.LOGS_FOLDER = self.log_location_path.get()
+        self.withdraw()
+        auto_ben.main()
+        self.deiconify()
+
+if __name__ == "__main__":
+    app = AutoBenGUI()
+    app.mainloop()
